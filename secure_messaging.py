ad0c79b8c46c3fea0389dbc2f10b0ba3936ded00
KazBrekker1 <justanothernerd000@gmail.com>
2021-11-25 16:22:59 +0300

    - added backkend to main

---
+++ secure_messaging.py
@@ -0,0 +1,152 @@
+from Crypto.Cipher import AES
+from Crypto.PublicKey import ECC
+from Crypto.Random import get_random_bytes
+import ecies
+import coincurve
+
+
+class User:
+    def __init__(self, name=None):
+        self._name = name
+        self.key = Key()
+        self._other_public_key = None
+
+    def sign(self, message):
+        return self.key.sign(message)
+
+    def verify(self, signature, message):
+        return coincurve.verify_signature(signature, message, self._other_public_key)
+
+    def encrypt(self, message):
+        # convert plaintext into bytes
+        data = message.encode()
+
+        # Path 1: plaintext -> SHA3-256 -> hash value -> asym encryption w/ private key -> signature
+        signature = self.sign(message)
+
+        # Path 2: plaintext -> sym encryption w/ shared key -> ciphertext
+        cipher = AES.new(self.key.shared, AES.MODE_CTR)
+        nonce = cipher.nonce
+        ciphertext = cipher.encrypt(data)
+
+        # After Path 1 & 2: signature || ciphertext -> signed ciphertext
+        signed_ciphertext = signature + ciphertext
+
+        return signed_ciphertext.hex(), nonce.hex(), len(signature)
+
+    def decrypt(self, received_message, nonce, sign_len):
+        data = bytes.fromhex(received_message)
+        nonce = bytes.fromhex(nonce)
+
+        signature = data[:sign_len]
+        ciphertext = data[sign_len:]
+
+        # ciphertext -> AES decryption w/ shared key -> plaintext
+        cipher = AES.new(self.key.shared, AES.MODE_CTR, nonce=nonce)
+        plaintext = cipher.decrypt(ciphertext)
+
+        # asym decryption w/ public key -> extracted hash == hash(plaintext) ?
+        is_secure = self.verify(signature, plaintext)
+
+        if is_secure:
+            print("The message is authentic.")
+            return plaintext.decode()
+        else:
+            print("The message is not authentic.")
+            return False
+
+    def send_key_exchange(self):
+        return self.key.send_key_exchange(self._other_public_key)
+
+    def receive_key_exchange(self, cipherkey):
+        self.key.receive_key_exchange(cipherkey)
+
+    @property
+    def other_public_key(self):
+        return self._other_public_key
+
+    @other_public_key.setter
+    def other_public_key(self, value):
+        if isinstance(value, str):
+            self._other_public_key = bytes.fromhex(value)
+        elif isinstance(value, (bytes, bytearray)):
+            self._other_public_key = value
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+class Key:
+    def __init__(self):
+        self._ecc_key = ECC.generate(curve="P-256")
+        self._coincurve_key = ecies.utils.generate_key().from_pem(
+            self._ecc_key.export_key(format="PEM").encode()
+        )
+        self._shared = None
+
+    @property
+    def private(self):
+        return self._private(format="bytes")
+
+    def _private(self, format="bytes"):
+        if format.lower() == "bytes":
+            return self._coincurve_key.secret
+        elif format.lower() == "pem":
+            return self._ecc_key.export_key(format="PEM")
+
+    @property
+    def public(self):
+        return self._public(format="bytes")
+
+    def _public(self, format="bytes"):
+        if format.lower() == "bytes":
+            return self._coincurve_key.public_key.format(True)
+        elif format.lower() == "pem":
+            return self._ecc_key.public_key().export_key(format="PEM")
+
+    @property
+    def shared(self):
+        return self._shared
+
+    def send_key_exchange(self, other_public_key):
+        self._shared = get_random_bytes(32)
+        return ecies.encrypt(other_public_key, self._shared).hex()
+
+    def receive_key_exchange(self, cipherkey):
+        self._shared = ecies.decrypt(self.private, bytes.fromhex(cipherkey))
+
+    def sign(self, message):
+        return self._coincurve_key.sign(message.encode())
+
+
+if __name__ == "__main__":
+    user1 = User("President Biden")
+    user2 = User("Sheikh Tamim")
+
+    # retrieving each other's public key
+    user1.other_public_key = user2.key.public.hex()
+    user2.other_public_key = user1.key.public.hex()
+
+    # let's say user1 initiates the key exchange
+    assert user1.key.shared is None and user2.key.shared is None  # proving no shared key initially
+    encrypted_shared_key = user1.send_key_exchange()
+    user2.receive_key_exchange(encrypted_shared_key)
+    # by this point, both of the users should have the same shared key
+    assert (
+        user1.key.shared is not None and user1.key.shared == user2.key.shared
+    )  # proving secret key is shared
+
+    # user2 wants to send a message
+    message = "Dear President Biden, Enter Islam and you will be safe."
+    ciphertext, nonce, sign_len = user2.encrypt(message)
+
+    # user1 receives and decrypts it
+    # if success = integrity, authencity and non-repudiation
+    # if error = at least one of them is not met
+    received_message = user1.decrypt(ciphertext, nonce, sign_len)
+    assert message == received_message  # proving the sent message is the same as the received one
+    print(user1.name, "received a message from", user2.name, ":", received_message)
---
+++ server.py
@@ -0,0 +1,99 @@
+from flask import Flask, send_from_directory, jsonify
+from flask_cors import CORS
+from flask import request
+from flask import jsonify
+from secure_messaging import User
+
+app = Flask(__name__)
+CORS(app)
+
+
+def res(data, status):
+    """
+    Named poorly to not type alot, originaly was create_response
+    """
+    return jsonify({"data": str(data), "status": status})
+
+
+# Path for our main Svelte page
+@app.route("/")
+def base():
+    return send_from_directory("client/public", "index.html")
+
+
+# Path for all the static files (compiled JS/CSS, etc.)
+@app.route("/<path:path>")
+def home(path):
+    return send_from_directory("client/public", path)
+
+
+@app.route("/set-name/<name>", methods=["GET", "POST"])
+def set_name(name):
+    user.name = name
+    return {"name": name, "status": "ok"}
+
+
+@app.route("/get-name/", methods=["GET", "POST"])
+def get_name():
+    if user.name is None:
+        return res("Username not set", "not-ok")
+    return res(user.name, "ok")
+
+
+################
+# Key Exchange #
+################
+# TO FRONTEND
+@app.route("/send-shared-key/", methods=["GET", "POST"])
+def send_shared_key():
+    return res(user.send_key_exchange(), "ok")
+
+
+# FROM FRONTEND
+@app.route("/other-pub-key/<other_pub_key>", methods=["GET", "POST"])
+def set_other_pub_key(other_pub_key):
+    user.other_public_key = other_pub_key
+    return res(other_pub_key, "ok")
+
+
+@app.route("/shared-key/<received_shared_key>/", methods=["GET", "POST"])
+def receive_shared_key(received_shared_key):
+    user.receive_key_exchange(received_shared_key)
+    return res(received_shared_key, "ok")
+
+
+######################
+# Message Encryption #
+######################
+# TO FRONTEND
+@app.route("/my-pub-key")
+def my_pub_key():
+    return res(user.key.public.hex(), "ok")
+
+
+@app.route("/encrypt-message", methods=["POST"])
+def encrypt_message():
+    message = request.json
+    ciphertext, nonce, sign_len = user.encrypt(message)
+    return {"ciphertext": ciphertext, "nonce": nonce, "sign_len": sign_len}
+
+
+######################
+# Message Decryption #
+######################
+# TO FRONTEND
+@app.route("/decrypt-message", methods=["POST"])
+def decrypt_message():
+    ciphertext = request.json["ciphertext"]
+    nonce = request.json["nonce"]
+    sign_len = request.json["sign_len"]
+    message = user.decrypt(ciphertext, nonce, sign_len)
+    if message:
+        return res(message, "ok")
+
+    return res("", "not-ok")
+
+
+if __name__ == "__main__":
+    user = User()
+    app.run()
